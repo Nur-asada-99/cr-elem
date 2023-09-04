46aa546be14368630f3496384ee6dbae2a567e32
Nur-asada-99 <126867367+Nur-asada-99@users.noreply.github.com>
2023-09-05 00:07:40 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,637 @@
+let body = document.body
+
+
+// главный блок в котором все
+
+let mine = document.createElement('div')
+body.append(mine)
+
+mine.style.width='1100px'
+mine.style.height='800px'
+
+
+mine.style.display='flex'
+mine.style.justifyContent='space-between'
+mine.style.flexWrap='wrap'
+
+
+
+// блок первый ==================================         1           ====================
+
+let blok_1 = document.createElement('div')
+
+blok_1.style.width='350px'
+blok_1.style.height='250px'
+blok_1.style.border='3px solid'
+blok_1.style.borderRadius='15px'
+
+mine.append(blok_1)
+
+let blok_1_time = document.createElement('div')
+let blok_1_time_h1 =document.createElement('h1')
+let blok_1_imag = document.createElement('div')
+let blok_1_imag_halp =document.createElement('div')
+let blok_1_imag_img =document.createElement('div')
+let blok_1_imag_h1 =document.createElement('h1')
+
+blok_1.append(blok_1_time)
+blok_1_time.append(blok_1_time_h1)
+
+blok_1.append(blok_1_imag)
+blok_1_imag.append(blok_1_imag_halp)
+blok_1_imag_halp.append(blok_1_imag_img)
+blok_1_imag_halp.append(blok_1_imag_h1)
+
+
+blok_1_imag_halp.style.width='150px'
+blok_1_imag_halp.style.height='150px'
+blok_1_imag_halp.style.display='flex'
+blok_1_imag_halp.style.justifyContent="center"
+blok_1_imag_halp.style.alignItems='center'
+blok_1_imag_halp.style.flexWrap='wrap'
+
+
+blok_1_time.style.width='350px'
+blok_1_time.style.height='50px'
+blok_1_time.style.borderBottom='3px solid'
+blok_1_time.style.display='flex'
+blok_1_time.style.justifyContent='center'
+blok_1_time.style.alignItems='center'
+blok_1_time.style.backgroundColor='rgb(82, 245, 245)'
+blok_1_time.style.borderRadius='11px 11px 0px 0px'
+
+blok_1_time_h1.innerHTML='7:25 AM'
+
+
+blok_1_imag.style.width='350px'
+blok_1_imag.style.height='200px'
+blok_1_imag.style.display='flex'
+blok_1_imag.style.justifyContent='center'
+blok_1_imag.style.alignItems='center'
+
+blok_1_imag_img.innerHTML='<img src="./takeoff.png" alt="">'
+
+blok_1_imag_h1.innerHTML='Boston'
+
+// важные мелочи
+blok_1.style.color='rgb(52, 185, 185)'
+
+// blok_1_time_h1.color='rgb(9, 99, 99)'
+// blok_1_imag_h1.color=' rgb(155, 245, 82)'
+
+// ================================================================
+
+
+
+
+// блок Второй ==============================          2          ============================
+
+let blok_2 = document.createElement('div')
+
+blok_2.style.width='350px'
+blok_2.style.height='250px'
+blok_2.style.border='3px solid'
+blok_2.style.borderRadius='15px'
+
+mine.append(blok_2)
+
+let blok_2_time = document.createElement('div')
+let blok_2_time_h1 =document.createElement('h1')
+let blok_2_imag = document.createElement('div')
+let blok_2_imag_halp =document.createElement('div')
+let blok_2_imag_img =document.createElement('div')
+let blok_2_imag_h1 =document.createElement('h1')
+
+blok_2.append(blok_2_time)
+blok_2_time.append(blok_2_time_h1)
+
+blok_2.append(blok_2_imag)
+blok_2_imag.append(blok_2_imag_halp)
+blok_2_imag_halp.append(blok_2_imag_img)
+blok_2_imag_halp.append(blok_2_imag_h1)
+
+
+blok_2_imag_halp.style.width='150px'
+blok_2_imag_halp.style.height='150px'
+blok_2_imag_halp.style.display='flex'
+blok_2_imag_halp.style.justifyContent="center"
+blok_2_imag_halp.style.alignItems='center'
+blok_2_imag_halp.style.flexWrap='wrap'
+
+
+blok_2_time.style.width='350px'
+blok_2_time.style.height='50px'
+blok_2_time.style.borderBottom='3px solid'
+blok_2_time.style.display='flex'
+blok_2_time.style.justifyContent='center'
+blok_2_time.style.alignItems='center'
+blok_2_time.style.backgroundColor='rgb(82, 245, 245)'
+blok_2_time.style.borderRadius='11px 11px 0px 0px'
+
+blok_2_time_h1.innerHTML='8:15 PM'
+
+
+blok_2_imag.style.width='350px'
+blok_2_imag.style.height='200px'
+blok_2_imag.style.display='flex'
+blok_2_imag.style.justifyContent='center'
+blok_2_imag.style.alignItems='center'
+
+blok_2_imag_img.innerHTML='<img src="./arrivals.png" alt="">'
+
+blok_2_imag_h1.innerHTML='Madrid'
+
+// важные мелочи
+blok_2.style.color='rgb(52, 185, 185)'
+
+// blok_1_time_h1.color='rgb(9, 99, 99)'
+// blok_1_imag_h1.color=' rgb(155, 245, 82)'
+
+// ================================================================
+
+
+
+
+
+// блок третий =====================          3          ============================
+
+let blok_3 = document.createElement('div')
+
+blok_3.style.width='350px'
+blok_3.style.height='250px'
+blok_3.style.border='3px solid'
+blok_3.style.borderRadius='15px'
+
+mine.append(blok_3)
+
+let blok_3_time = document.createElement('div')
+let blok_3_time_h1 =document.createElement('h1')
+let blok_3_imag = document.createElement('div')
+let blok_3_imag_halp =document.createElement('div')
+let blok_3_imag_img =document.createElement('div')
+let blok_3_imag_h1 =document.createElement('h1')
+
+blok_3.append(blok_3_time)
+blok_3_time.append(blok_3_time_h1)
+
+blok_3.append(blok_3_imag)
+blok_3_imag.append(blok_3_imag_halp)
+blok_3_imag_halp.append(blok_3_imag_img)
+blok_3_imag_halp.append(blok_3_imag_h1)
+
+
+blok_3_imag_halp.style.width='150px'
+blok_3_imag_halp.style.height='150px'
+blok_3_imag_halp.style.display='flex'
+blok_3_imag_halp.style.justifyContent="center"
+blok_3_imag_halp.style.alignItems='center'
+blok_3_imag_halp.style.flexWrap='wrap'
+
+
+blok_3_time.style.width='350px'
+blok_3_time.style.height='50px'
+blok_3_time.style.borderBottom='3px solid'
+blok_3_time.style.display='flex'
+blok_3_time.style.justifyContent='center'
+blok_3_time.style.alignItems='center'
+blok_3_time.style.backgroundColor='rgb(82, 245, 245)'
+blok_3_time.style.borderRadius='11px 11px 0px 0px'
+
+blok_3_time_h1.innerHTML='Translate'
+
+
+blok_3_imag.style.width='350px'
+blok_3_imag.style.height='200px'
+blok_3_imag.style.display='flex'
+blok_3_imag.style.justifyContent='center'
+blok_3_imag.style.alignItems='center'
+
+blok_3_imag_img.innerHTML='<img src="./translate.png" alt="">'
+
+blok_3_imag_h1.innerHTML='Spanish'
+
+// важные мелочи
+blok_3.style.color='rgb(52, 185, 185)'
+
+// blok_1_time_h1.color='rgb(9, 99, 99)'
+// blok_1_imag_h1.color=' rgb(155, 245, 82)'
+
+// ================================================================
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>    2     <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+
+// блок  ==================================         4           ====================
+
+let blok_4 = document.createElement('div')
+
+blok_4.style.width='350px'
+blok_4.style.height='250px'
+blok_4.style.border='3px solid'
+blok_4.style.borderRadius='15px'
+
+mine.append(blok_4)
+
+let blok_4_time = document.createElement('div')
+let blok_4_time_h1 =document.createElement('h1')
+let blok_4_imag = document.createElement('div')
+let blok_4_imag_halp =document.createElement('div')
+let blok_4_imag_img =document.createElement('div')
+let blok_4_imag_h1 =document.createElement('h1')
+
+blok_4.append(blok_4_time)
+blok_4_time.append(blok_4_time_h1)
+
+blok_4.append(blok_4_imag)
+blok_4_imag.append(blok_4_imag_halp)
+blok_4_imag_halp.append(blok_4_imag_img)
+blok_4_imag_halp.append(blok_4_imag_h1)
+
+
+blok_4_imag_halp.style.width='150px'
+blok_4_imag_halp.style.height='150px'
+blok_4_imag_halp.style.display='flex'
+blok_4_imag_halp.style.justifyContent="center"
+blok_4_imag_halp.style.alignItems='center'
+blok_4_imag_halp.style.flexWrap='wrap'
+
+
+blok_4_time.style.width='350px'
+blok_4_time.style.height='50px'
+blok_4_time.style.borderBottom='3px solid'
+blok_4_time.style.display='flex'
+blok_4_time.style.justifyContent='center'
+blok_4_time.style.alignItems='center'
+blok_4_time.style.backgroundColor='rgb(248, 255, 152)'
+blok_4_time.style.borderRadius='11px 11px 0px 0px'
+
+blok_4_time_h1.innerHTML='Bike'
+
+
+blok_4_imag.style.width='350px'
+blok_4_imag.style.height='200px'
+blok_4_imag.style.display='flex'
+blok_4_imag.style.justifyContent='center'
+blok_4_imag.style.alignItems='center'
+
+blok_4_imag_img.innerHTML='<img src="./bike.png" alt="">'
+
+blok_4_imag_h1.innerHTML='13.1 Miles'
+
+// важные мелочи
+blok_4.style.color='rgb(245, 168, 1)'
+
+// blok_1_time_h1.color='rgb(9, 99, 99)'
+// blok_1_imag_h1.color=' rgb(155, 245, 82)'
+
+// ================================================================
+
+
+
+
+// блок  ==============================          5          ============================
+
+let blok_5 = document.createElement('div')
+
+blok_5.style.width='350px'
+blok_5.style.height='250px'
+blok_5.style.border='3px solid'
+blok_5.style.borderRadius='15px'
+
+mine.append(blok_5)
+
+let blok_5_time = document.createElement('div')
+let blok_5_time_h1 =document.createElement('h1')
+let blok_5_imag = document.createElement('div')
+let blok_5_imag_halp =document.createElement('div')
+let blok_5_imag_img =document.createElement('div')
+let blok_5_imag_h1 =document.createElement('h1')
+
+blok_5.append(blok_5_time)
+blok_5_time.append(blok_5_time_h1)
+
+blok_5.append(blok_5_imag)
+blok_5_imag.append(blok_5_imag_halp)
+blok_5_imag_halp.append(blok_5_imag_img)
+blok_5_imag_halp.append(blok_5_imag_h1)
+
+
+blok_5_imag_halp.style.width='160px'
+blok_5_imag_halp.style.height='150px'
+blok_5_imag_halp.style.display='flex'
+blok_5_imag_halp.style.justifyContent="center"
+blok_5_imag_halp.style.alignItems='center'
+blok_5_imag_halp.style.flexWrap='wrap'
+
+
+blok_5_time.style.width='350px'
+blok_5_time.style.height='50px'
+blok_5_time.style.borderBottom='3px solid'
+blok_5_time.style.display='flex'
+blok_5_time.style.justifyContent='center'
+blok_5_time.style.alignItems='center'
+blok_5_time.style.backgroundColor='rgb(248, 255, 152)'
+blok_5_time.style.borderRadius='11px 11px 0px 0px'
+
+blok_5_time_h1.innerHTML='Boat'
+
+
+blok_5_imag.style.width='350px'
+blok_5_imag.style.height='200px'
+blok_5_imag.style.display='flex'
+blok_5_imag.style.justifyContent='center'
+blok_5_imag.style.alignItems='center'
+
+blok_5_imag_img.innerHTML='<img src="./boat.png" alt="">'
+
+blok_5_imag_h1.innerHTML='90 Minutes'
+
+// важные мелочи
+blok_5.style.color='rgb(245, 168, 1)'
+
+// blok_1_time_h1.color='rgb(9, 99, 99)'
+// blok_1_imag_h1.color=' rgb(155, 245, 82)'
+
+// ================================================================
+
+
+
+
+
+// блок 6 =====================          6          ============================
+
+let blok_6 = document.createElement('div')
+
+blok_6.style.width='350px'
+blok_6.style.height='250px'
+blok_6.style.border='3px solid'
+blok_6.style.borderRadius='15px'
+
+mine.append(blok_6)
+
+let blok_6_time = document.createElement('div')
+let blok_6_time_h1 =document.createElement('h1')
+let blok_6_imag = document.createElement('div')
+let blok_6_imag_halp =document.createElement('div')
+let blok_6_imag_img =document.createElement('div')
+let blok_6_imag_h1 =document.createElement('h1')
+
+blok_6.append(blok_6_time)
+blok_6_time.append(blok_6_time_h1)
+
+blok_6.append(blok_6_imag)
+blok_6_imag.append(blok_6_imag_halp)
+blok_6_imag_halp.append(blok_6_imag_img)
+blok_6_imag_halp.append(blok_6_imag_h1)
+
+
+blok_6_imag_halp.style.width='150px'
+blok_6_imag_halp.style.height='150px'
+blok_6_imag_halp.style.display='flex'
+blok_6_imag_halp.style.justifyContent="center"
+blok_6_imag_halp.style.alignItems='center'
+blok_6_imag_halp.style.flexWrap='wrap'
+
+
+blok_6_time.style.width='350px'
+blok_6_time.style.height='50px'
+blok_6_time.style.borderBottom='3px solid'
+blok_6_time.style.display='flex'
+blok_6_time.style.justifyContent='center'
+blok_6_time.style.alignItems='center'
+blok_6_time.style.backgroundColor='rgb(248, 255, 152)'
+blok_6_time.style.borderRadius='11px 11px 0px 0px'
+
+blok_6_time_h1.innerHTML='Drive'
+
+
+blok_6_imag.style.width='350px'
+blok_6_imag.style.height='200px'
+blok_6_imag.style.display='flex'
+blok_6_imag.style.justifyContent='center'
+blok_6_imag.style.alignItems='center'
+
+blok_6_imag_img.innerHTML='<img src="./sports_car.png" alt="">'
+
+blok_6_imag_h1.innerHTML='48 Miles'
+
+// важные мелочи
+blok_6.style.color='rgb(245, 168, 1)'
+
+// blok_1_time_h1.color='rgb(9, 99, 99)'
+// blok_1_imag_h1.color=' rgb(155, 245, 82)'
+
+// ================================================================
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>     3     <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+
+// блок  ==================================         7           ====================
+
+let blok_7 = document.createElement('div')
+
+blok_7.style.width='350px'
+blok_7.style.height='250px'
+blok_7.style.border='3px solid'
+blok_7.style.borderRadius='15px'
+
+mine.append(blok_7)
+
+let blok_7_time = document.createElement('div')
+let blok_7_time_h1 =document.createElement('h1')
+let blok_7_imag = document.createElement('div')
+let blok_7_imag_halp =document.createElement('div')
+let blok_7_imag_img =document.createElement('div')
+let blok_7_imag_h1 =document.createElement('h1')
+
+blok_7.append(blok_7_time)
+blok_7_time.append(blok_7_time_h1)
+
+blok_7.append(blok_7_imag)
+blok_7_imag.append(blok_7_imag_halp)
+blok_7_imag_halp.append(blok_7_imag_img)
+blok_7_imag_halp.append(blok_7_imag_h1)
+
+
+blok_7_imag_halp.style.width='150px'
+blok_7_imag_halp.style.height='150px'
+blok_7_imag_halp.style.display='flex'
+blok_7_imag_halp.style.justifyContent="center"
+blok_7_imag_halp.style.alignItems='center'
+blok_7_imag_halp.style.flexWrap='wrap'
+
+
+blok_7_time.style.width='350px'
+blok_7_time.style.height='50px'
+blok_7_time.style.borderBottom='3px solid'
+blok_7_time.style.display='flex'
+blok_7_time.style.justifyContent='center'
+blok_7_time.style.alignItems='center'
+blok_7_time.style.backgroundColor='rgb(255, 158, 162)'
+blok_7_time.style.borderRadius='11px 11px 0px 0px'
+
+blok_7_time_h1.innerHTML='Morning'
+
+
+blok_7_imag.style.width='350px'
+blok_7_imag.style.height='200px'
+blok_7_imag.style.display='flex'
+blok_7_imag.style.justifyContent='center'
+blok_7_imag.style.alignItems='center'
+
+blok_7_imag_img.innerHTML='<img src="./morning.png" alt="">'
+
+blok_7_imag_h1.innerHTML='Coffee'
+
+// важные мелочи
+blok_7.style.color='rgb(139, 12, 17)'
+
+// blok_1_time_h1.color='rgb(9, 99, 99)'
+// blok_1_imag_h1.color=' rgb(155, 245, 82)'
+
+// ================================================================
+
+
+
+
+// блок  ==============================          8          ============================
+
+let blok_8 = document.createElement('div')
+
+blok_8.style.width='350px'
+blok_8.style.height='250px'
+blok_8.style.border='3px solid'
+blok_8.style.borderRadius='15px'
+
+mine.append(blok_8)
+
+let blok_8_time = document.createElement('div')
+let blok_8_time_h1 =document.createElement('h1')
+let blok_8_imag = document.createElement('div')
+let blok_8_imag_halp =document.createElement('div')
+let blok_8_imag_img =document.createElement('div')
+let blok_8_imag_h1 =document.createElement('h1')
+
+blok_8.append(blok_8_time)
+blok_8_time.append(blok_8_time_h1)
+
+blok_8.append(blok_8_imag)
+blok_8_imag.append(blok_8_imag_halp)
+blok_8_imag_halp.append(blok_8_imag_img)
+blok_8_imag_halp.append(blok_8_imag_h1)
+
+
+blok_8_imag_halp.style.width='130px'
+blok_8_imag_halp.style.height='150px'
+blok_8_imag_halp.style.display='flex'
+blok_8_imag_halp.style.justifyContent="center"
+blok_8_imag_halp.style.alignItems='center'
+blok_8_imag_halp.style.flexWrap='wrap'
+
+
+blok_8_time.style.width='350px'
+blok_8_time.style.height='50px'
+blok_8_time.style.borderBottom='3px solid'
+blok_8_time.style.display='flex'
+blok_8_time.style.justifyContent='center'
+blok_8_time.style.alignItems='center'
+blok_8_time.style.backgroundColor='rgb(255, 158, 162)'
+blok_8_time.style.borderRadius='11px 11px 0px 0px'
+
+blok_8_time_h1.innerHTML='Afternoon'
+
+
+blok_8_imag.style.width='350px'
+blok_8_imag.style.height='200px'
+blok_8_imag.style.display='flex'
+blok_8_imag.style.justifyContent='center'
+blok_8_imag.style.alignItems='center'
+
+blok_8_imag_img.innerHTML='<img src="./gym.png" alt="">'
+
+blok_8_imag_h1.innerHTML='Gym'
+
+// важные мелочи
+blok_8.style.color='rgb(139, 12, 17)'
+
+// blok_1_time_h1.color='rgb(9, 99, 99)'
+// blok_1_imag_h1.color=' rgb(155, 245, 82)'
+
+// ================================================================
+
+
+
+
+
+// блок  =====================          9          ============================
+
+let blok_9 = document.createElement('div')
+
+blok_9.style.width='350px'
+blok_9.style.height='250px'
+blok_9.style.border='3px solid'
+blok_9.style.borderRadius='15px'
+
+mine.append(blok_9)
+
+let blok_9_time = document.createElement('div')
+let blok_9_time_h1 =document.createElement('h1')
+let blok_9_imag = document.createElement('div')
+let blok_9_imag_halp =document.createElement('div')
+let blok_9_imag_img =document.createElement('div')
+let blok_9_imag_h1 =document.createElement('h1')
+
+blok_9.append(blok_9_time)
+blok_9_time.append(blok_9_time_h1)
+
+blok_9.append(blok_9_imag)
+blok_9_imag.append(blok_9_imag_halp)
+blok_9_imag_halp.append(blok_9_imag_img)
+blok_9_imag_halp.append(blok_9_imag_h1)
+
+
+blok_9_imag_halp.style.width='150px'
+blok_9_imag_halp.style.height='150px'
+blok_9_imag_halp.style.display='flex'
+blok_9_imag_halp.style.justifyContent="center"
+blok_9_imag_halp.style.alignItems='center'
+blok_9_imag_halp.style.flexWrap='wrap'
+
+
+blok_9_time.style.width='350px'
+blok_9_time.style.height='50px'
+blok_9_time.style.borderBottom='3px solid'
+blok_9_time.style.display='flex'
+blok_9_time.style.justifyContent='center'
+blok_9_time.style.alignItems='center'
+blok_9_time.style.backgroundColor='rgb(255, 158, 162)'
+blok_9_time.style.borderRadius='11px 11px 0px 0px'
+
+blok_9_time_h1.innerHTML='Night'
+
+
+blok_9_imag.style.width='350px'
+blok_9_imag.style.height='200px'
+blok_9_imag.style.display='flex'
+blok_9_imag.style.justifyContent='center'
+blok_9_imag.style.alignItems='center'
+
+blok_9_imag_img.innerHTML='<img src="./hot_tub.png" alt="">'
+
+blok_9_imag_h1.innerHTML='Hot Tub'
+
+// важные мелочи
+blok_9.style.color='rgb(139, 12, 17)'
+
+// blok_1_time_h1.color='rgb(9, 99, 99)'
+// blok_1_imag_h1.color=' rgb(155, 245, 82)'
+
+
+// created by m.r Sultan
+
+// ================================================================
+
